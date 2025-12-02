23f55e973ec1869fd752dba4ba6db9b3b5836ac7
Hardik-06-bit <hs7465969@gmail.com>
2025-12-02 16:16:03 +0530

    Auto-sync: file changes

--- datatypes.js
+++ datatypes.js
@@ -2,3 +2,22 @@
 
 //alert(3+3)// we are using nodejs,not browser
 
+//datatypes
+let name = "hardik" //string
+let age= 18 //number
+let isloggedin= true //boolean
+
+
+//number > 2 to power 53
+//boolean > true/false
+//string > "xyz"
+//bigint
+//null > standalone value
+//undefined >
+//symbol > unique
+
+
+//object
+
+
+console.log(typeof null) // type of null is object...
